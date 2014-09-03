fa2918d4bb7bfa69166ebe54e472f10a3146b4f0
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-03 22:08:32 +0900

    Remove feeds

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140903121120) do
 
-  create_table "feeds", force: true do |t|
-    t.string   "title"
-    t.text     "content"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "facebook_id"
     t.string   "email"
