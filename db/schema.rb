aad5c037dd0d32abba8a1b3bd97db84d336fa9a2
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-03 21:02:53 +0900

    Execute 'rake db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903082508) do
+ActiveRecord::Schema.define(version: 20140903112440) do
+
+  create_table "feeds", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "facebook_id"
