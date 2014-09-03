33ee946327dd72fdb90e08e78c2d94fcb8ac1d7e
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-03 21:02:07 +0900

    Remove action new to feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :feeds
 
   root to: "home#index"
 
+  resources :feeds, except: [:new]
   resources :users
 
   get '/auth/:provider/callback' => 'users#callback'
