1e25ba034624abd44fe8cf2b8d5371be387e4150
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-03 21:39:30 +0900

    Modify current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
 
   def current_user
     if cookies[:token]
@@ -7,5 +8,9 @@ class ApplicationController < ActionController::Base
     else
       redirect_to root_path
     end
+  end
+
+  def logged_in?
+    current_user != nil
   end
 end
