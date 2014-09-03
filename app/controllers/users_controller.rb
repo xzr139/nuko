7a90b6aab8a171f42542732099eff199ca9edbcd
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-03 18:50:24 +0900

    Write callback methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def callback
+    User.set_data_from_omnifb_info(request.env['omniauth.auth'])
+    redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  def self.set_data_from_omnifb_info(fb_info)
+    where(facebook_id: fb_info['uid']).first_or_create(
+      facebook_id:  fb_info['uid'],
+      email:        fb_info['info']['email'],
+      full_name:    fb_info['extra']["raw_info"]['name'],
+      token:        fb_info['credentials']['token']
+    )
+  end
 end
