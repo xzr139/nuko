9eca974a047adb15e1732e0c6110c338f1ee746d
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-03 20:12:42 +0900

    Change routing method from resource resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   resources :home, only: [:index]
-  resource :users
+  resources :users
 
   get '/auth/:provider/callback' => 'users#callback'
 end
