9ea8b555729d54dd30dcfb192ccdd757cfbd0f9d
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-03 21:15:23 +0900

    Execute 'rake db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903082508) do
+ActiveRecord::Schema.define(version: 20140903121120) do
+
+  create_table "feeds", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "facebook_id"
@@ -22,6 +30,10 @@ ActiveRecord::Schema.define(version: 20140903082508) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "company"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
