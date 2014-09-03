4cc49ae43dc810ad4915ef186974b14a8b81f3b5
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-03 21:15:49 +0900

    Add validation for use in image upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+  has_attached_file :avatar,
+    styles: { medium: "300x300>", thumb: "100x100>" },
+    default_url: "/images/:style/missing.png"
+
+  validates_attachment_content_type :avatar,
+    content_type: /\Aimage\/.*\Z/
+
   def self.set_data_from_omnifb_info(fb_info)
     where(facebook_id: fb_info['uid']).first_or_create(
       facebook_id:  fb_info['uid'],
