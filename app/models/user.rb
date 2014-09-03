2d2b471883fe61b7f837cf5c09f5535003ed4919
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-03 23:49:46 +0900

    Add relation user and feeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :feeds
+
   def self.set_data_from_omnifb_info(fb_info)
     where(facebook_id: fb_info['uid']).first_or_create(
       facebook_id:  fb_info['uid'],
