4c8c852e7166d640c903062dde917ca4c080e089
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-03 20:11:41 +0900

    Create user detail update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
   before_action :current_user, except: [:callback]
+  before_action :user_params, only: [:update]
+
+  def update
+    @current_user.update(user_params)
+    redirect_to root_path, notice: t('users.update.complate_update_nick_name')
+  end
 
   def callback
     user = User.set_data_from_omnifb_info(request.env['omniauth.auth'])
     cookies[:token] = user.token
     redirect_to edit_user_path(user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:nick_name)
   end
 end
