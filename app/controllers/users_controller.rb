a9e65bdb04a7fa29ef5656e15f87ef35d546b443
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-03 20:10:17 +0900

    Fix callback method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   before_action :current_user, except: [:callback]
 
   def callback
-    token = User.set_data_from_omnifb_info(request.env['omniauth.auth']).token
-    cookies[:token] = token
-    redirect_to root_path
+    user = User.set_data_from_omnifb_info(request.env['omniauth.auth'])
+    cookies[:token] = user.token
+    redirect_to edit_user_path(user.id)
   end
 end
