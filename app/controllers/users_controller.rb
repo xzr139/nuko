35d7fc843b9034c592dfa7a90acf72b536f44be5
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-03 18:51:38 +0900

    Write callback methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def callback
-    User.set_data_from_omnifb_info(request.env['omniauth.auth'])
+    token = User.set_data_from_omnifb_info(request.env['omniauth.auth']).token
+    cookies[:token] = token
     redirect_to root_path
   end
 end
