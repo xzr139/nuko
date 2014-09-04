e6df8d06d3bcc2571378266826a65b866b4941bb
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-05 06:06:33 +0900

    Remove unuse module

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,6 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
   config.include FactoryGirl::Syntax::Methods
-  config.include OmniAuthMacros
 
   config.before do
     FactoryGirl.reload
