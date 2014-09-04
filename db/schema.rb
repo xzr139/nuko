bf1c5d6063724ef26356fa83fafb8b74f2b1ed53
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-05 04:59:36 +0900

    Execute 'rake db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140904014054) do
+ActiveRecord::Schema.define(version: 20140904033557) do
 
   create_table "notes", force: true do |t|
     t.string   "title"
@@ -36,4 +36,5 @@ ActiveRecord::Schema.define(version: 20140904014054) do
     t.datetime "avatar_updated_at"
     t.text     "bio"
   end
+
 end
