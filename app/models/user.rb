5f1772c8365833360919eb262e45de859ce65ed3
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-04 12:15:39 +0900

    Fix thumb size

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
  has_attached_file :avatar,
-   styles: { medium: "300x300>", thumb: "100x100>" },
+   styles: { medium: "300x300>", thumb: "20x20>" },
    default_url: "/images/default_image.png"
 
   validates_attachment_content_type :avatar,
