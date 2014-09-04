d20352481824720e7eac7507469bcd2103cfa832
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-05 07:40:35 +0900

    Write test for notes of show

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -4,11 +4,22 @@ describe NotesController, type: :request do
   context "index" do
     before do
       12.times { create(:note) }
-      get '/notes'
+      get "/notes"
     end
 
     it "should be return is 10" do
       expect(assigns[:notes].count).to eq(10)
+    end
+  end
+
+  context "show" do
+    before do
+      12.times { create(:note) }
+      get "/notes/" + Note.first.id.to_s
+    end
+
+    it "should be return is 10" do
+      expect(assigns[:note]).to eq(Note.first)
     end
   end
 end
