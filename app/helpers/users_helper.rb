465f33f1b60ce3c3f2f2838d84702e14f9b202b0
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-05 03:43:21 +0900

    Set global image style

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
   def profile_image(user, size = '30x30')
-    image_tag(user.avatar.url, size: size, alt: 'icon')
+    image_tag(user.avatar.url, class: 'changeable-size-image', size: size, alt: 'icon')
   end
 end
