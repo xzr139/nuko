ac0949d876a783abba853c5e0f24daf41c75a913
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-04 09:57:45 +0900

    Add set_locale method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
+  before_action :set_locale
 
   def current_user
     if cookies[:token]
-      @current_user = User.where(token: cookies[:token]).first
+      @current_user = user.where(token: cookies[:token]).first
     else
       redirect_to root_path
     end
@@ -12,5 +13,14 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     current_user != nil
+  end
+
+  def default_url_options(options={})
+    { :locale => I18n.locale }
+  end
+
+  # リンクの多言語化に対応する
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
