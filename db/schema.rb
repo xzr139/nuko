ad95770b9f12b80c499044e391f633e88c4534f8
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-04 10:44:24 +0900

    Execute 'rake db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903112440) do
+ActiveRecord::Schema.define(version: 20140904014307) do
 
   create_table "feeds", force: true do |t|
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "notes", force: true do |t|
     t.string   "title"
     t.text     "content"
+    t.integer  "feed_id"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
