a9f3c266f33ca0d53511f5e791a4601cf8c910fc
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-05 18:49:13 +0900

    Change test method from describe to context

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -21,64 +21,45 @@ describe NotesController, type: :controller do
     it "should be return is selected id" do
       expect(assigns[:note]).to eq(Note.last)
     end
-  end
 
-  describe "with valid params" do
-    before do
-      @user = create(:user)
-      @params = { note: attributes_for(:note) }
-      ApplicationController.any_instance.stub(:current_user).and_return(@user)
-    end
+  describe "POST create"
+    context "with valid params" do
+      before do
+        @user = create(:user)
+        @params = { note: attributes_for(:note) }
+        ApplicationController.any_instance.stub(:current_user).and_return(@user)
+      end
 
-    it "creates a new note" do
-      expect {
+      it "creates a new note" do
+        expect {
+          post :create, { note: @params }
+        }.to change(Note, :count).by(1)
+      end
+
+      it "redirects to the created note" do
         post :create, { note: @params }
-      }.to change(Note, :count).by(1)
+        expect(response).to redirect_to(Note.last)
+      end
     end
 
-    it "redirects to the created note" do
-      post :create, { note: @params }
-      expect(response).to redirect_to(Note.last)
-    end
-  end
+    context "with invalid params" do
+      before do
+        @user = create(:user)
+        @params = { note: attributes_for(:note) }
+        ApplicationController.any_instance.stub(:current_user).and_return(@user)
+      end
 
-  describe "with invalid params" do
-    before do
-      @user = create(:user)
-      @params = { note: attributes_for(:note) }
-      ApplicationController.any_instance.stub(:current_user).and_return(@user)
-    end
+      it "assigns a newly created but unsaved note as @note" do
+        Note.any_instance.stub(:save).and_return(false)
+        post :create, { note: @params }
+        expect(assigns(:note)).to be_a_new(Note)
+      end
 
-    it "assigns a newly created but unsaved note as @note" do
-      Note.any_instance.stub(:save).and_return(false)
-      post :create, { note: @params }
-      expect(assigns(:note)).to be_a_new(Note)
-    end
-
-    it "re-renders the 'new' template" do
-      Note.any_instance.stub(:save).and_return(false)
-      post :create, { note: @params }
-      expect(response).to render_template("index")
-    end
-  end
-
-  describe "with invalid params" do
-    before do
-      @user = create(:user)
-      @params = { note: attributes_for(:note) }
-      ApplicationController.any_instance.stub(:current_user).and_return(@user)
-    end
-
-    it "assigns a newly created but unsaved note as @note" do
-      Note.any_instance.stub(:save).and_return(false)
-      post :create, { note: @params }
-      expect(assigns(:note)).to be_a_new(Note)
-    end
-
-    it "re-renders the 'new' template" do
-      Note.any_instance.stub(:save).and_return(false)
-      post :create, { note: @params }
-      expect(response).to render_template("index")
+      it "re-renders the 'new' template" do
+        Note.any_instance.stub(:save).and_return(false)
+        post :create, { note: @params }
+        expect(response).to render_template("index")
+      end
     end
   end
 end
