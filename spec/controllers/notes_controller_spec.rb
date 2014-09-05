7863fa87c6bb9305ecc4d1a8725409d3b9ac1e60
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-05 19:40:45 +0900

    Write spec for show

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -61,5 +61,50 @@ describe NotesController, type: :controller do
         expect(response).to render_template("index")
       end
     end
+
+  describe 'PUT updatee'
+    let(:request) { put :update, params}
+
+    context 'type valid value' do
+      let(:note) { create(:note)}
+      let(:params) { { id: note.id, note: attributes_for(:note) } }
+
+      it 'should be sccess' do
+        request
+        expect(response).to redirect_to(Note.last)
+      end
+
+      it 'is in agreement with what note to edit chose' do
+        request
+        expect(assigns(:note)).to eq(note)
+      end
+
+      it 'should be no error' do
+        request
+        expect(assigns(:note).errors).to be_empty
+      end
+    end
+
+    context 'type unvalid value' do
+      let(:note) { create(:note)}
+      let(:title) { 'タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル' }
+      let(:params) { { id: note.id, note: attributes_for(:note, title: title) } }
+
+      it 'should be success and render edit' do
+        request
+        expect(response.status).to eq(200)
+        expect(response).to render_template('edit')
+      end
+
+      it 'is in agreement with what note to edit chose' do
+        request
+        expect(assigns(:note)).to eq(note)
+      end
+
+      it 'should be validation erorr' do
+        request
+        expect(assigns(:note).errors).not_to be_empty
+      end
+    end
   end
 end
