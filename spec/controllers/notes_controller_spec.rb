3f1b65f79226ba520b46d1c98b870b2aef9c3501
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-05 18:25:51 +0900

    Create test for note of create

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -22,4 +22,42 @@ describe NotesController, type: :controller do
       expect(assigns[:note]).to eq(Note.first)
     end
   end
+  describe "with valid params" do
+    before do
+      @user = create(:user)
+      @params = { note: attributes_for(:note) }
+      ApplicationController.any_instance.stub(:current_user).and_return(@user)
+    end
+
+    it "creates a new note" do
+      expect {
+        post :create, { note: @params }
+      }.to change(Note, :count).by(1)
+    end
+
+    it "redirects to the created note" do
+      post :create, { note: @params }
+      expect(response).to redirect_to(Note.last)
+    end
+  end
+
+  describe "with invalid params" do
+    before do
+      @user = create(:user)
+      @params = { note: attributes_for(:note) }
+      ApplicationController.any_instance.stub(:current_user).and_return(@user)
+    end
+
+    it "assigns a newly created but unsaved note as @note" do
+      Note.any_instance.stub(:save).and_return(false)
+      post :create, { note: @params }
+      expect(assigns(:note)).to be_a_new(Note)
+    end
+
+    it "re-renders the 'new' template" do
+      Note.any_instance.stub(:save).and_return(false)
+      post :create, { note: @params }
+      expect(response).to render_template("index")
+    end
+  end
 end
