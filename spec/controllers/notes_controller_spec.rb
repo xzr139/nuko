a7a00810d5974afec79a7b6bd46e539cdf5db47a
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-05 18:31:10 +0900

    Fix show spec

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -12,14 +12,14 @@ describe NotesController, type: :controller do
     end
   end
 
-  context "show" do
+  context "GET show" do
     before do
-      12.times { create(:note) }
-      get :show, { id: Note.first.id.to_s }
+      note = create(:note)
+      get :show, { id: note.to_param }
     end
 
     it "should be return is selected id" do
-      expect(assigns[:note]).to eq(Note.first)
+      expect(assigns[:note]).to eq(Note.last)
     end
   end
 
@@ -39,6 +39,26 @@ describe NotesController, type: :controller do
     it "redirects to the created note" do
       post :create, { note: @params }
       expect(response).to redirect_to(Note.last)
+    end
+  end
+
+  describe "with invalid params" do
+    before do
+      @user = create(:user)
+      @params = { note: attributes_for(:note) }
+      ApplicationController.any_instance.stub(:current_user).and_return(@user)
+    end
+
+    it "assigns a newly created but unsaved note as @note" do
+      Note.any_instance.stub(:save).and_return(false)
+      post :create, { note: @params }
+      expect(assigns(:note)).to be_a_new(Note)
+    end
+
+    it "re-renders the 'new' template" do
+      Note.any_instance.stub(:save).and_return(false)
+      post :create, { note: @params }
+      expect(response).to render_template("index")
     end
   end
 
