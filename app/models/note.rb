8634e3eb275ab9b701ab0e7ae470ddb7ca7050af
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-08 14:33:07 +0900

    Add relations

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,7 @@
 class Note < ActiveRecord::Base
   belongs_to :user
+  belongs_to :stock
+
   validates :title,  length: { maximum: 43 }
   acts_as_taggable
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,4 @@
 class Stock < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :note
 end
