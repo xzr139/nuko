98d658510ee447aca5f068a15095a62c37371d58
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-08 14:26:06 +0900

    Execute 'rake db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140907021309) do
+ActiveRecord::Schema.define(version: 20140908051800) do
 
   create_table "notes", force: true do |t|
     t.string   "title"
     t.text     "content"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "stocks", force: true do |t|
+    t.integer  "note_id"
+    t.integer  "user_id"
+    t.boolean  "stoked"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
