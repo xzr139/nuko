56b5a413217a184097aaccaa26804b1224e5738a
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-08 15:59:42 +0900

    Create stock function

--- app/assets/javascripts/notes.js.coffee
+++ app/assets/javascripts/notes.js.coffee
@@ -1,1 +1,10 @@
 
+$ ->
+  $('.btn.btn-primary.stock-button.stock-bt').click ->
+    $.ajax
+      url: "/#{location.pathname.split('/')[1]}/stocks"
+      type: 'POST'
+      data:
+        note_id: $(this).attr('data-note-id')
+        user_id: $(this).attr('data-user-id')
+      success: (data) ->
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,10 @@
 class StocksController < ApplicationController
 
   def create
+    @stock = current_user.stocks.where(note_id: params[:note_id], user_id: params[:user_id]).build
+    @stock.save unless Stock.exists?(note_id: params[:note_id], user_id: params[:user_id])
+
+    redirect_to note_path(@stock.note)
   end
 
   def update
