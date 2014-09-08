99f8a8aeb6bfb8f01bdfbc46b77047843c60f885
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-08 16:17:07 +0900

    Fix stock create code

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,10 +1,10 @@
 class StocksController < ApplicationController
 
   def create
-    @stock = current_user.stocks.where(note_id: params[:note_id], user_id: params[:user_id]).build
-    @stock.save unless Stock.exists?(note_id: params[:note_id], user_id: params[:user_id])
+    stock = current_user.stocks.build( note_id: params[:note_id], stocked: true)
+    stock.save unless Stock.exists?(note_id: params[:note_id], user_id: params[:user_id], stocked: true)
 
-    redirect_to note_path(@stock.note)
+    redirect_to note_path(stock.note)
   end
 
   def update
