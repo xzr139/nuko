71315aae61624d1ebb74e1880ec898006e27cdb4
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-08 21:43:45 +0900

    Fixed indent

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,7 @@
 class StocksController < ApplicationController
 
   def create
-    stock = current_user.stocks.build( note_id: params[:note_id], stocked: true)
+    stock = current_user.stocks.build(note_id: params[:note_id], stocked: true)
     stock.save unless Stock.exists?(note_id: params[:note_id], user_id: params[:user_id], stocked: true)
     redirect_to note_path(stock.note)
   end
