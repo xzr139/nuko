81c9501190e1d3ca604fc980574bb1894ec6ca3b
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-09 00:05:44 +0900

    Fixed all routing
    Conflicts:
      config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,21 @@ Rails.application.routes.draw do
   root to: 'notes#index'
   get '/:locale' => 'notes#index'
 
-  scope "/:locale" do
-    resources :books
-    resources :notes, except: [:new] do
+  scope path: "(:locale)", shallow_path: "(:locale)", locale: /en|ja/ do
+    resources :notes, except: [:new] , shallow: true do
       collection do
         get 'tag'
       end
     end
 
-    resources :users do
+    resources :users, shallow: true do
       member do
         get 'all_posts'
         get 'tag'
       end
     end
+
+    resource :stocks, only: [:create, :update], shallow: true
   end
 
   get '/auth/:provider/callback' => 'users#callback'
