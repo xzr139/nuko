438d178a0ca099ee6acd695db2b4b3e63cd0a222
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-09 23:18:18 +0900

    Add example for update of stocks

--- spec/controllers/stocks_controller_spec.rb
+++ spec/controllers/stocks_controller_spec.rb
@@ -43,4 +43,28 @@ describe StocksController, type: :controller do
       end
     end
   end
+
+  describe 'PATCH update' do
+    context 'type valid value' do
+
+      before do
+        @stock = create(:stock, stocked: true)
+        @user = @stock.user
+        ApplicationController.any_instance.stub(:current_user).and_return(@user)
+        patch :update, { note_id: @stock.note_id }
+      end
+
+      it 'should be sccess' do
+        expect(response).to redirect_to(@stock.note)
+      end
+
+      it 'is in agreement with what note to edit chose' do
+        expect(assigns(:stock)).to eq(Stock.last)
+      end
+
+      it 'should be no error' do
+        expect(assigns(:stock).errors).to be_empty
+      end
+    end
+  end
 end
