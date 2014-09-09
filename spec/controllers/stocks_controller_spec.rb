00b3d4d5fc2251cc612a2901dac8c8476896c7d2
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-09 22:16:00 +0900

    Add example for create

--- spec/controllers/stocks_controller_spec.rb
+++ spec/controllers/stocks_controller_spec.rb
@@ -1,4 +1,25 @@
 require 'spec_helper'
 
 describe StocksController, type: :controller do
+  describe "POST create" do
+    context "with valid params" do
+      let(:user) { create(:user) }
+      let(:note_id) { create(:note).id }
+
+      before do
+        ApplicationController.any_instance.stub(:current_user).and_return(user)
+      end
+
+      it "creates a new note" do
+        expect {
+          post :create, { note_id: note_id }
+        }.to change(Stock, :count).by(1)
+      end
+
+      it "redirects to the created note" do
+        post :create, { note_id: note_id }
+        expect(response).to redirect_to(Note.last)
+      end
+    end
+  end
 end
