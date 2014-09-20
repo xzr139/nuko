7dac4862c99be2ceb1d01f807c5fff8af12398cb
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-20 21:37:45 +0900

    Refactor for 'stock update'

--- spec/controllers/stocks_controller_spec.rb
+++ spec/controllers/stocks_controller_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe StocksController, type: :controller do
   describe 'PATCH update' do
     context 'type valid value' do
+      let!(:stock) { create(:stock) }
+
       before do
-        @stock = create(:stock, stocked: true)
-        @user = @stock.user
-        ApplicationController.any_instance.stub(:current_user).and_return(@user)
-        patch :update, { note_id: @stock.note_id }
+        ApplicationController.any_instance.stub(:current_user).and_return(User.last)
+        patch :update, { note_id: stock.note_id }
       end
 
       it 'should be blank' do
