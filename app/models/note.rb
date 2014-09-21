fc7691b169e0010f291e4aa1c372f19a8e744773
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-21 21:50:18 +0900

    Execute 'annotate'

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  content    :text
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Note < ActiveRecord::Base
   belongs_to :user
   has_many :stocks
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: stocks
+#
+#  id         :integer          not null, primary key
+#  note_id    :integer          not null
+#  user_id    :integer          not null
+#  stocked    :boolean          default(FALSE)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Stock < ActiveRecord::Base
   include PublicActivity::Model
 
