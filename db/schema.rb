dd9aaf47c2e31e224e2ea7db92d0881567f85669
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-28 23:12:27 +0900

    Execute 'rake db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140921130256) do
+ActiveRecord::Schema.define(version: 20140928141036) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20140921130256) do
     t.string   "recipient_type"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "readed",         default: false
+    t.boolean  "unread",         default: false
   end
 
   add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type", using: :btree
