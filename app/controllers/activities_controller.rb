e9580c4de2032934e838e7d189fd628ef88cbed9
Satoshi Ohmori <sachin21dev@gmail.com>
2014-09-28 23:37:18 +0900

    Create read function

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,4 +4,12 @@ class ActivitiesController < ApplicationController
   def index
     @activities = PublicActivity::Activity.order(created_at: :desc).where(recipient_id: current_user.id, owner_type: "User").where.not(owner_id: current_user.id)
   end
+
+  def update
+    if Activity.exists?(id: params[:id])
+      Activity.find(params[:id]).update(unread: false)
+    end
+
+    render nothing: true
+  end
 end
