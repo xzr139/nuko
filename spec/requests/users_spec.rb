00526ef45cd8cb3451aa39e627457b2ffc197071
Satoshi Ohmori <sachin21dev@gmail.com>
2014-10-08 10:46:22 +0900

    Write edit user for spec

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+describe "Users", type: :request do
+  context "GET edit" do
+    let(:user) { create(:user) }
+    let(:edited_user) { User.last }
+
+    before do
+      ApplicationController.any_instance.stub(:current_user).and_return(user)
+      visit edit_user_path(user)
+      fill_in "user_nick_name",              with: "sachin21edited"
+      fill_in "user_bio",                    with: "edited bio"
+      select User::INTERFACE_LANGUAGE[0][0], from: "user_language"
+      click_button "submit-user"
+    end
+
+    it "should be success edit" do
+      expect(edited_user.nick_name).to eq("sachin21edited")
+      expect(edited_user.bio).to eq("edited bio")
+      expect(edited_user.language).to eq("ja")
+    end
+  end
+end
