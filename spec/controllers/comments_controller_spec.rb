fd5a01ce650baf0e9577452dbc65cd1967763424
Satoshi Ohmori <sachin21dev@gmail.com>
2014-10-10 15:58:22 +0900

    Write comment spec

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,4 +1,17 @@
 require "spec_helper"
 
 describe CommentsController, type: :controller do
+  describe "POST create" do
+    let(:user) { create(:user) }
+    let(:note) { create(:note) }
+    before do
+      ApplicationController.any_instance.stub(:current_user).and_return(user)
+    end
+
+    it "should be success create" do
+      expect { post :create,  note_id: note.id, comment: attributes_for(:comment) }.to change {
+        Comment.count
+      }.from(0).to(1)
+    end
+  end
 end
