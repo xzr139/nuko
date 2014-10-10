66148befe5c7c56b66285eead7f845eba7f10dac
Satoshi Ohmori <sachin21dev@gmail.com>
2014-10-10 16:04:03 +0900

    Write note request spec

---
+++ spec/requests/notes_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+
+describe "Notes", type: :request do
+  describe "GET /notes" do
+    let(:note) { create(:note) }
+
+    it "should be success page load" do
+      get note_path(note)
+      expect(response.status).to be(200)
+    end
+  end
+end
