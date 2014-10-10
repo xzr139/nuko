2fc2ad3a230b21f1d7c3fd15f708f23ebec9e565
Satoshi Ohmori <sachin21dev@gmail.com>
2014-10-10 16:57:12 +0900

    Add number of comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def simple_pluralize(count, singular, plural=nil)
+    if locale == :en
+      ((count == 1 || count =~ /^1(\.0+)?$/) ? singular : (plural || singular.pluralize))
+    else
+      singular
+    end
+  end
 end
