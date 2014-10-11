8e642ba98308fcbd5c82ae9724a9035a6a33568d
Satoshi Ohmori <sachin21dev@gmail.com>
2014-10-11 14:44:31 +0900

    Fixed notes spec

--- spec/requests/notes_spec.rb
+++ spec/requests/notes_spec.rb
@@ -11,19 +11,12 @@ describe "Notes", type: :request do
   end
 
   describe "POST create" do
-    let(:note) { create(:note) }
-
     before do
       visit "/auth/facebook"
       ApplicationController.any_instance.stub(:current_user).and_return(User.last)
     end
 
-    it "should be none notes", js: true do
-      visit root_path
-      expect(page).not_to have_css("div.block")
-    end
-
-    it "should be success create", js: true do
+    it "should be success create and destroy", js: true do
       visit root_path
       fill_in "note_title",        with: "title"
       fill_in "note_content",      with: "content"
