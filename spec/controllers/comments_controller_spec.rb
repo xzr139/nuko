4b922674235797511e74b969eb45aa0da84f6367
Satoshi Ohmori <sachin21dev@gmail.com>
2014-10-14 09:29:01 +0900

    Fixed spec bugs

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -2,14 +2,13 @@ require "spec_helper"
 
 describe CommentsController, type: :controller do
   describe "POST create" do
-    let(:user) { create(:user) }
     let(:note) { create(:note) }
     before do
-      ApplicationController.any_instance.stub(:current_user).and_return(user)
+      ApplicationController.any_instance.stub(:current_user).and_return(note.user)
     end
 
     it "should be success create" do
-      expect { post :create,  note_id: note.id, comment: attributes_for(:comment) }.to change {
+      expect { post :create, comment: attributes_for(:comment).merge({ user_id: note.user.id,  note_id: note.id }) }.to change {
         Comment.count
       }.from(0).to(1)
     end
