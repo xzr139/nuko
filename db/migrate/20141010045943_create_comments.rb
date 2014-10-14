f11854d0f6e73ce612143608f3715bc98a9eb299
Satoshi Ohmori <sachin21dev@gmail.com>
2014-10-14 17:44:07 +0900

    Add indices and default value

--- db/migrate/20141010045943_create_comments.rb
+++ db/migrate/20141010045943_create_comments.rb
@@ -1,12 +1,14 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.integer    :user_id
-      t.integer    :note_id
-      t.string     :content
-      t.integer    :like_count
-      t.datetime   :deleted_at
+      t.integer    :user_id,     null: false
+      t.integer    :note_id,     null: false
+      t.string     :content,     null: false
+      t.integer    :like_count,  null: false, default: 0
       t.timestamps
+
+      t.index :user_id
+      t.index :note_id
     end
   end
 end
