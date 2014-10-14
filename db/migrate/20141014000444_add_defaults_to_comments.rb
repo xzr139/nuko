e4f673d470e037dbd81ff6cebd46bb5623503afd
Satoshi Ohmori <sachin21dev@gmail.com>
2014-10-14 09:37:50 +0900

    Revert "Execute 'rails g migration add_defaults_to_comments'"
    This reverts commit c13939e810d9f18a1e0553891cf672d64de7889b.

--- db/migrate/20141014000444_add_defaults_to_comments.rb
+++
@@ -1,8 +0,0 @@
-class AddDefaultsToComments < ActiveRecord::Migration
-  def change
-    change_column :comments, :note_id,    :integer, null: false
-    change_column :comments, :user_id,    :integer, null: false
-    change_column :comments, :content,    :text,    null: false
-    change_column :comments, :like_count, :integer, default: 0
-  end
-end
