9e153224f771959242e2c80ab3e0fa73f033f11c
Satoshi Ohmori <sachin21dev@gmail.com>
2014-12-03 01:38:45 +0900

    Revert "Execute 'rails g model follow'"
    This reverts commit d5222a7c1d2a53e52b2a209dadb68c4044153b66.

--- db/migrate/20141202162415_create_follows.rb
+++
@@ -1,9 +0,0 @@
-class CreateFollows < ActiveRecord::Migration
-  def change
-    create_table :follows do |t|
-      t.integer     :user_id
-      t.integer     :target_user_id
-      t.timestamps
-    end
-  end
-end
