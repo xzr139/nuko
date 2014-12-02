2b34264e0bb88c055163a0746a799f42223313fe
Satoshi Ohmori <sachin21dev@gmail.com>
2014-12-03 01:42:29 +0900

    Execute 'rake db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024120006) do
+ActiveRecord::Schema.define(version: 20141202163902) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 20141024120006) do
 
   add_index "comments", ["note_id"], name: "index_comments_on_note_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "followers", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "target_user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "likes", force: true do |t|
     t.integer  "note_id",                    null: false
