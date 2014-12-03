0b73a928e39d20c7e6be152728fd7b9ffebdd395
Satoshi Ohmori <sachin21dev@gmail.com>
2014-12-03 23:35:27 +0900

    Fixed typo

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,7 +1,7 @@
 class FollowersController < ApplicationController
   def update
-    @follow = current_user.followers.find_by(user_id: params[:user_id])
-    if current_user.followers.exists?(user_id: params[:user_id])
+    @follow = current_user.followers.find_by(target_user_id: params[:user_id])
+    if current_user.followers.exists?(target_user_id: params[:user_id])
       @follow.update(followed: current_user.followers.exists?(target_user_id: params[:user_id], followed: false))
     else
       @follow = current_user.followers.build(target_user_id: params[:user_id], followed: true)
