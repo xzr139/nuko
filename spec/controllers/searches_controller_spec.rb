e95fb7d22f0a02f305fd055ff788a9c126a1d66c
Satoshi Ohmori <sachin21dev@gmail.com>
2014-12-08 06:57:59 +0900

    Write spec for note search

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 
 describe SearchesController, type: :controller do
+  context "GET index" do
+    before do
+      12.times { create(:note) }
+      create(:note, content: 'hogeeee')
+      get :index, content: 'hoge'
+    end
+
+    it 'should be return 200' do
+      expect(response).to be_success
+    end
+
+    it 'should be return selected query only note' do
+      expect(assigns[:notes].count).to eq(1)
+      expect(assigns[:notes].first.content).to eq('hogeeee')
+    end
+  end
 end
