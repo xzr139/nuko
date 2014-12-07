89acc0d4ae10c0ac7a1a520c62607fc30fdc6727
Satoshi Ohmori <sachin21dev@gmail.com>
2014-12-08 05:54:15 +0900

    Created search function, but this function is not complited

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,5 @@
 class SearchesController < ApplicationController
+  def index
+    @notes = Note.search(content_cont: params['content']).result().page(params[:page]).per(10).order(created_at: :desc)
+  end
 end
