f8f8e46ab86dcbe5e22a92895408cbff282897e5
Satoshi Ohmori <sachin21dev@gmail.com>
2014-12-08 06:58:58 +0900

    Fixed typo

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,5 @@
 class SearchesController < ApplicationController
   def index
-    @notes = Note.page(params[:page]).per(10).order(created_at: :desc).search(content_cont: params['content']).result()  end
+    @notes = Note.page(params[:page]).per(10).order(created_at: :desc).search(content_cont: params['content']).result()
+  end
 end
