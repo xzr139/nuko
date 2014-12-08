d2fca7a6a3c4f26ec1fcb840ca6825f4779e41f2
Satoshi Ohmori <sachin21dev@gmail.com>
2014-12-09 02:01:03 +0900

    Do not use parentheses for method calls with no arguments

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,5 @@
 class SearchesController < ApplicationController
   def index
-    @notes = Note.page(params[:page]).per(10).order(created_at: :desc).search(content_cont: params['content']).result()
+    @notes = Note.page(params[:page]).per(10).order(created_at: :desc).search(content_cont: params['content']).result
   end
 end
