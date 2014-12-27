2a6f26c53bfcef22c9b6f9225e9e4fdd322ad3a0
Satoshi Ohmori <sachin21dev@gmail.com>
2014-12-27 18:06:12 +0900

    Set title for note

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,2 +1,9 @@
 module NotesHelper
+  def title
+    if (params['controller'] == 'notes' || params['controller'] == 'comments') && params['id'].present?
+      @note.title ? @note.title : Settings.service.name
+    else
+      Settings.service.name
+    end
+  end
 end
