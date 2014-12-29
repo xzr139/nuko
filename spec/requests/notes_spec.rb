5d71f6fcc96324b6a27d7fa59a168730237034c6
Satoshi Ohmori <sachin21dev@gmail.com>
2014-12-29 12:20:12 +0900

    Write spec for note header

--- spec/requests/notes_spec.rb
+++ spec/requests/notes_spec.rb
@@ -1,19 +1,35 @@
 require "spec_helper"
 
 describe "Notes", type: :request do
+  def create_note
+    visit root_path
+    fill_in "note_title",        with: "title"
+    fill_in "note_content",      with: "# hoge"
+    fill_in "note_tag_list",     with: "tag1 tag2"
+    click_button "submit-note"
+  end
+
   describe "POST create" do
     before do
       visit "/auth/facebook"
       ApplicationController.any_instance.stub(:current_user).and_return(User.last)
+      create_note
     end
 
     it "should be success create and destroy", js: true do
-      visit root_path
-      fill_in "note_title",        with: "title"
-      fill_in "note_content",      with: "content"
-      fill_in "note_tag_list",     with: "tag1 tag2"
-      click_button "submit-note"
-      expect(page).to have_content("content")
+      expect(page).to have_content("hoge")
+    end
+  end
+
+  describe "GET show" do
+    before do
+      visit "/auth/facebook"
+      ApplicationController.any_instance.stub(:current_user).and_return(User.last)
+      create_note
+    end
+
+    it "should see h1 tag", js: true do
+      expect(page.body).to include("<h1>hoge</h1>")
     end
   end
 end
