f2f88f0c913f44a35ea36c6981f454377d5a3147
Satoshi Ohmori <sachin21dev@gmail.com>
2014-12-31 22:11:56 +0900

    Remove unneeded code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -83,7 +83,6 @@ class ApplicationController < ActionController::Base
 
   def render_500(e = nil)
     logger.info "Rendering 500 with exception: #{e.message}" if e
-    Airbrake.notify(e) if e
 
     if request.xhr?
       render json: { error: '500 error' }, status: 500
