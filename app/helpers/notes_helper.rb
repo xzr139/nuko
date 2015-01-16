fcb4baf4327caa2f59d8337f6401490f23766649
Satoshi Ohmori <sachin21dev@gmail.com>
2015-01-17 01:23:44 +0900

    Add condition for nick name into title method

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -4,6 +4,8 @@ module NotesHelper
 
     if (params['controller'] == 'notes' || params['controller'] == 'comments') && params['id'].present? && @note
       @note.title ? "#{@note.title} - #{name}" : name
+    elsif params['controller'] == 'profiles' && params['id'].present?
+      User.find(params[:id]).nick_name + ' - ' + name
     else
       t("common.home", name: name)
     end
