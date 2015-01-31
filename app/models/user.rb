c8c57889a05e743d443074d428e7ddbd4027b39c
Satoshi Ohmori <sachin21dev@gmail.com>
2015-01-31 22:25:14 +0900

    Add available setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook], authentication_keys: [:login]
 
   INTERFACE_LANGUAGE = [
     %w(日本語 ja),
