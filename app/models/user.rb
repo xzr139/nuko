516d029e0047fdf57ed8a1658a91fea5a6c53072
Satoshi Ohmori <sachin21dev@gmail.com>
2015-01-31 22:19:45 +0900

    Add value in 'user.rb'

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,6 +52,8 @@ class User < ActiveRecord::Base
   validates :bio, length: { maximum: 300 }
   validates :email, presence: true
 
+  attr_accessor :login
+
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "20x20>" }, default_url: "/images/default_image.png"
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
