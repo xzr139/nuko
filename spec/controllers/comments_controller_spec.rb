b2f5d8a1fd4b13f7cbbd9e0207979c8e1f3008a9
Satoshi Ohmori <sachin21dev@gmail.com>
2015-01-31 13:45:13 +0900

    Remove unused 'before do ~ end'

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -5,9 +5,6 @@ describe CommentsController, type: :controller do
     let(:note) { create(:note) }
     let(:ids_hash) { { user_id: note.user.id,   note_id: note.id } }
 
-    before do
-    end
-
     it "should be success create" do
       expect { post :create, comment: attributes_for(:comment).merge(ids_hash) }.to change {
         Comment.count
