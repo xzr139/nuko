4d7100553aaa984425cc1e96997b580cbb2f9698
Satoshi Ohmori <sachin21dev@gmail.com>
2015-02-07 22:17:05 +0900

    Write spec for stocks

--- spec/controllers/stocks_controller_spec.rb
+++ spec/controllers/stocks_controller_spec.rb
@@ -19,16 +19,28 @@ describe StocksController, type: :controller do
         expect(assigns(:stock).errors).to be_empty
       end
     end
-  end
 
-  context 'it should be increment number of activity count' do
-    let(:user) { create(:user) }
-    let(:note) { create(:note) }
+    context 'update test' do
+      let(:note) { create(:note) }
 
-    it 'should be success create_activity' do
-      expect { patch :update, note_id: note.id }.to change {
-        Activity.count
-      }.from(0).to(1)
+      it 'should increment stocks' do
+        expect { patch :update, note_id: note.id }.to change {
+          Stock.count
+        }.from(0).to(1)
+      end
+    end
+
+    context 'update test' do
+      let!(:stock) { create(:stock, stocked: true) }
+
+      before do
+        ApplicationController.any_instance.stub(:current_user).and_return(stock.user)
+        patch :update, note_id: Note.last.id
+      end
+
+      it 'should update followed' do
+        expect(assigns[:stock].stocked).to eq(false)
+      end
     end
   end
 end
