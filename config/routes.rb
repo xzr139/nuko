6dab3dcb9fd13c8ad2675784ca9c1b016e65a8d3
Satoshi Ohmori <sachin21dev@gmail.com>
2015-02-12 19:54:44 +0900

    Execute 'rails g controller tags'

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     resources :searches, only: [:index], shallow: true
     resources :users, only: [:edit, :update], shallow: true
     resources :activities, only: [:index, :update], shallow: true
+    resources :tags, only: [:index, :show], shallow: true
 
     resources :profiles, only: [:show, :update, :edit], shallow: true do
       member do
---
+++ spec/controllers/tags_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe TagsController, type: :controller do
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
