41fdbb85a1473455b432047e2721a216c3de815e
Satoshi Ohmori <sachin21dev@gmail.com>
2015-02-24 20:10:22 +0900

    Install bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,12 @@ Rails.application.configure do
 
   # For devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+  end
 end
