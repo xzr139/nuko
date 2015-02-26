a0d955aaa210953650e9fe3120f5c6e7fadf1dc8
Satoshi Ohmori <sachin21dev@gmail.com>
2015-02-27 00:08:56 +0900

    Fix spec

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,10 +1,10 @@
 class FollowersController < ApplicationController
   def update
-    @follow = current_user.followers.find_by(target_id: params[:target_id])
+    @follow = current_user.followers.find_by(target_id: params[:user_id])
     if current_user.followers.exists?(target_id: params[:user_id])
-      @follow.update(followed: current_user.followers.exists?(target_id: params[:target_id], followed: false))
+      @follow.update(followed: current_user.followers.exists?(target_id: params[:user_id], followed: false))
     else
-      @follow = current_user.followers.build(target_id: params[:target_id], followed: true)
+      @follow = current_user.followers.build(target_id: params[:user_id], followed: true)
       @follow.create_follow current_user
       @follow.create_activity :create, owner: current_user, recipient: User.find(@follow.target_id)
     end
