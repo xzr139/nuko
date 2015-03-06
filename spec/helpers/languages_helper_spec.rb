40bfbe7d00a90dc594563c7e1298cc653eed2c35
Satoshi Ohmori <sachin21dev@gmail.com>
2015-03-07 06:23:17 +0900

    Write spec for language_selector

--- spec/helpers/languages_helper_spec.rb
+++ spec/helpers/languages_helper_spec.rb
@@ -1,4 +1,9 @@
 require 'spec_helper'
 
 describe LanguagesHelper, type: :helper do
+  describe "language selector" do
+    it "returns same value" do
+      expect(helper.language_selector).to eq(Language.pluck(:name, :locale))
+    end
+  end
 end
