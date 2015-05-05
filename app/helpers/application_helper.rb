4d3e17f9f851d7ce1e61205d3cf30c6ae4099a3b
Satoshi Ohmori <sachin21dev@gmail.com>
2015-05-05 14:03:50 +0900

    Support singular and plural

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,4 +23,12 @@ module ApplicationHelper
       singular
     end
   end
+
+  def target_with_pluralize(count, sentence, target_word, plural = nil)
+    if locale == 'en' || locale == :en
+      sentence.gsub(target_word, ((count == 1 || count =~ /^1(\.0+)?$/) ? target_word : (plural || target_word.pluralize)))
+    else
+      sentence
+    end
+  end
 end
