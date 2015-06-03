847b1801145b1598d85a5646f3fe91066c7e02bf
Satoshi Ohmori <sachin21dev@gmail.com>
2015-06-03 19:59:17 +0900

    Fix English title in tags controller spec

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -1,36 +1,36 @@
 require 'spec_helper'
 
 describe TagsController, type: :controller do
-  describe "GET index" do
+  describe "GET #index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'PATCH follow' do
+  describe 'PATCH #follow' do
     let(:tag) { create(:tag) }
 
-    context 'with type valid value' do
+    context 'when type valid value' do
       let!(:follow_activities) { create(:follow_activities) }
 
       before { patch :follow, id: tag.id, target_id: tag.id }
 
-      it 'should be blank' do
+      it 'has no error' do
         expect(response.body).to be_blank
       end
 
-      it 'is in agreement with what note to edit chose' do
+      it 'same @follow as a FollowActivity last value' do
         expect(assigns(:follow)).to eq(FollowActivity.last)
       end
 
-      it 'should be no error' do
+      it 'has no erorr' do
         expect(assigns(:follow).errors).to be_empty
       end
     end
 
     context 'with new follower' do
-      it 'should followed' do
+      it 'will be created FollowActivity' do
         expect { patch :follow, id: tag.id, target_id: tag.id }.to change {
           FollowActivity.count
         }.from(0).to(1)
@@ -43,16 +43,16 @@ describe TagsController, type: :controller do
         patch :follow, id: tag.id, target_id: tag.id
       end
 
-      it 'should be not blank' do
+      it 'has no error' do
         expect(response.body).to be_blank
       end
 
-      it 'should be no error' do
+      it 'has no error' do
         expect(assigns(:follow).errors).to be_empty
       end
     end
 
-    context 'follow test' do
+    context 'when follow at test' do
       let(:tag) { create(:tag) }
       let(:follow_activities) { create(:follow_activities, target_id: tag.id, followed: true) }
 
@@ -61,7 +61,7 @@ describe TagsController, type: :controller do
         patch :follow, id: tag.id, target_id: tag.id
       end
 
-      it 'should be followed' do
+      it 'has been followed' do
         expect(assigns[:follow].followed).to eq(false)
       end
     end
