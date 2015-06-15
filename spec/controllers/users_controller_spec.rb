2bbd17998a885864c2d31f8eb65a6a9c3cb0c36c
Satoshi Ohmori <sachin21dev@gmail.com>
2015-06-15 14:33:50 +0000

    Fix English users controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,14 +1,14 @@
 require "spec_helper"
 
 describe UsersController,  type: :controller do
-  describe "GET edit" do
+  describe "GET #edit" do
     let(:user) { create(:user) }
 
     before do
       get :edit, id: user.id
     end
 
-    it "should be success" do
+    it "returns success status code" do
       expect(response.status).to eq(200)
     end
   end
