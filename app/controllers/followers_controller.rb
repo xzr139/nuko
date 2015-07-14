0b48cb008cc2c87b77302c2bc509fbb8eea1de54
Satoshi Ohmori <sachin21dev@gmail.com>
2015-07-14 20:34:48 +0900

    Use transaction

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -3,17 +3,23 @@ class FollowersController < ApplicationController
     @follow = current_user.follow_activities.find_by(target_id: params[:user_id])
     @follower = current_user.followers.find_by(target_id: params[:user_id])
 
-    if current_user.follow_activities.exists?(target_id: params[:user_id]) && current_user.followers.exists?(target_id: params[:user_id])
-      @follow.update(followed: current_user.follow_activities.exists?(target_id: params[:user_id], followed: false))
-      @follower.update(followed: current_user.follow_activities.exists?(target_id: params[:user_id], followed: false))
-    else
-      @follow = current_user.follow_activities.build(target_id: params[:user_id], followed: true)
-      @follow.create_follow current_user
-      @follow.create_activity :create, owner: current_user, recipient: User.find(@follow.target_id)
-      @follower = current_user.followers.build(target_id: params[:user_id], followed: true)
-      @follower.save
-    end
+    ActiveRecord::Base.transaction do
+      if current_user.follow_activities.exists?(target_id: params[:user_id]) && current_user.followers.exists?(target_id: params[:user_id])
+        @follow.update!(followed: current_user.follow_activities.exists?(target_id: params[:user_id], followed: false))
+        @follower.update!(followed: current_user.follow_activities.exists?(target_id: params[:user_id], followed: false))
+      else
+        @follow = current_user.follow_activities.build(target_id: params[:user_id], followed: true)
+        @follow.create_follow current_user
+        @follow.create_activity :create, owner: current_user, recipient: User.find(@follow.target_id)
+        @follower = current_user.followers.build(target_id: params[:user_id], followed: true)
+        @follower.save!
+      end
 
-    render nothing: true, status: :ok
+      render nothing: true, status: :ok
+    end
+  rescue => ex
+    logger.debug ex.message
+    logger.debug ex.backtrace.join("\n")
+    render json: { follow_error: @follow.errors, follower_error: @follower.errors }, status: :unprocessable_entity
   end
 end
--- app/models/concerns/create_follow.rb
+++ app/models/concerns/create_follow.rb
@@ -1,6 +1,6 @@
 module CreateFollow
   def create_follow(model)
     self.follow_type = model.class.name
-    save
+    save!
   end
 end
