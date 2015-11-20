050af00765ad74ff38cac2cc7232a48cb2467d98
Satoshi Ohmori <sachin21dev@gmail.com>
2015-11-20 17:03:42 +0900

    Fix a coding style

--- spec/controllers/users/users_controller_spec.rb
+++ spec/controllers/users/users_controller_spec.rb
@@ -3,10 +3,7 @@ require "spec_helper"
 describe UsersController, type: :controller do
   context "GET edit" do
     let(:user) { create(:user) }
-
-    before do
-      get :edit, id: user.id
-    end
+    before { get :edit, id: user.id }
 
     it "should be success" do
       expect(response.status).to eq(200)
